97558dc6ee086c8ac52db61975d8c59441a5545b
Alexion <alex24bryant@mail.ru>
2022-04-06 13:49:37 +0500

    fix test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -12,23 +12,6 @@ def test_stack():
     assert stack.pop() == 'one'
 
 
-def test_stack1():
-    stack = []
-    stack.append('one')
-    stack.append('two')
-
-    assert stack.pop() == 'two'
-
-
-def test_stack2():
-    stack = []
-    stack.append('one')
-    stack.append('two')
-
-    stack.pop()
-    assert stack.pop() == 'one'
-
-
 def test_emptiness():
     stack = []
     assert not stack
